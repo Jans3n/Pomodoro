34df535a42768dd5d42310b12c9692e4761a0446
Jans3n <jans3nsu@gmail.com>
2024-01-02 15:30:03 +0100

    Initial setup
    made button component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CustomButton  from './components/customButton'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>test</h1>
+        <CustomButton buttonName="Start" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/customButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function customButton({buttonName}) {
+  return (
+    <>
+      <div>
+        <button>{buttonName}</button>
+      </div>
+    </>
+  )
+}
+export default customButton
